72ecc774515de04039627ffe09281eb638eb27a1
Vegah92 <mondivegah83@gmail.com>
2023-03-27 18:39:58 -0700

    myprintf.c

--- myprintf.c
+++ myprintf.c
@@ -1,67 +1,66 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
-*_printf - entry point
-*@format:function parameter
-*/
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
 
-while (*format)
-{
-if (*format == '%')
-{
-format++;
+if (format == NULL)
+return (-1);
 
-if (*format == 'd')
+va_start(list, format);
+
+for (i = 0; format && format[i] != '\0'; i++)
 {
-int num = va_arg(args, int);
-char str[32];
-myitoa(num, str, 10);
-myputs(str);
-}
-else if (*format == 's')
+if (format[i] != '%')
 {
-char *str = va_arg(args, char*);
-myputs(str);
-}
-else if (*format == 'c')
-{
-int ch = va_arg(args, int);
-myputchar((char) ch);
-}
-else if (*format == 'f')
-{
-double d = va_arg(args, double);
-int whole = (int) d;
-int decimal = (int) ((d - whole) * 1000000);
-char str[32];
-myitoa(whole, str, 10);
-myputs(str);
-myputchar('.');
-myitoa(decimal, str, 10);
-myputs(str);
-}
-else if (*format == 'x')
-{
-int num = va_arg(args, int);
-char str[32];
-myitoa(num, str, 16);
-myputs(str);
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
 }
 else
 {
-/* handle other format specifiers*/
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
 }
-}
-else
-{
-myputchar(*format);
-}
-format++;
-}
-va_end(args);
-return (0);
 }
 
+print_buffer(buffer, &buff_ind);
+
+va_end(list);
+
+return (printed_chars);
+}
+
+/**
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
+}
